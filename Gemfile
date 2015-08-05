498b25271933ba232da78ce64d067fd8b579e3fb
FrackenToaster <frakentoaster@gmail.com>
2015-08-05 13:58:23 -0700

    Added User and devise

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'simple_form', '~> 3.1.0'
 gem 'bootstrap-sass', '~> 3.3.5.1'
 gem 'paperclip', '~> 4.3.0'
 gem 'cocoon', '~> 1.2.6'
+gem 'devise', '~> 3.5.1'
 
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ActiveRecord::Base
+	belongs_to :user
 
 	has_many :ingredients
 	has_many :directions
@@ -19,4 +20,3 @@ class Recipe < ActiveRecord::Base
   has_attached_file :image, styles: { :medium => "400x400#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
- 
